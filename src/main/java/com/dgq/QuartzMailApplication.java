107c34423c832b97008547572098995b0c96b02e
dengGQ <565820745@qq.com>
2019-05-23 10:12:12 +0800

    master 的新提交

--- src/main/java/com/dgq/QuartzMailApplication.java
+++ src/main/java/com/dgq/QuartzMailApplication.java
@@ -14,5 +14,7 @@ public class QuartzMailApplication {
 		System.out.print("---------");
 		System.out.print("---------dgq");
 		System.out.print("-----------new Branch");
+		System.out.print("-----------mastre");
+		
 	}
 }
